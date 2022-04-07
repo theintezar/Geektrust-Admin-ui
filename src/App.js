f72c9d75da2ccd190e8a8121266cfed50f164284
Intezar Ahmad <31982418+theintezar@users.noreply.github.com>
2022-04-07 11:35:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import "./App.css"
+
+
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData(setData);
+  }, []);
+
+  const deleteUser = (id) => {
+    let tempUsers = data.filter((user) => user.id !== id);
+    setData(tempUsers);
+  };
+
+
+  const api = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+
+  const getData = ()=> {
+    fetch(api)
+    .then((d) => d.json())
+    .then((res)=> {
+      setData(res);
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="inputdiv"><input className="inputbar" type="text" placeholder="search by name email or role" /></div>
+      <div className="headingdiv">
+        <div>Select</div>
+        <div>Name</div>
+        <div>Email</div>
+        <div>Role</div>
+        <div>Actions</div>
+      </div>
+      <hr />
+
+      {data.map((e)=> (
+        <div className="maindiv" key={e.id}> 
+            <div><input type="checkbox" /></div>
+            <div>{e.name}</div>
+            <div>{e.email}</div>
+            <div>{e.role}</div>
+          
+            <button onClick={()=>{
+            deleteUser(e.id)
+          }}>Delete</button>
+          </div>
+      ))}
     </div>
   );
 }
